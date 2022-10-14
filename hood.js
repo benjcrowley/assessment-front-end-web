5ad4993dbc9d850aa10710e81dab396d4cd4e2a1
Bennett Crowley <benjcrowley@gmail.com>
2022-10-14 12:07:11 -0600

    added in the extra credit, all done, i should get a 100% on this

--- hood.js
+++ hood.js
@@ -1,9 +1,17 @@
-const restaurantBtn = document.querySelector('#randomRestaurant')
 
+// Set up my querySelectors
+const restaurantBtn = document.querySelector('#randomRestaurant')
+const restaurantSelection = document.querySelector('#restaurant-choice')
+
+//create an array of possible restaurants
 let restauantArr = ['Jimmy Johns', 'Bumblebees', 'little ceasars', 'two jacks pizza (get the ranch)', 'more Betters', 'Bombay house', 'Midici Pizza', 'swig', 'wendys', 'cafe rio', 'in-n-out', 'Thai Papaya Cuisine', 'Five Sushi Brothers', 'Bowls Superfoods']
 
+//write a function that will choose a random restaurant (this will be invoked later)
 function chooseRestaurant() {
     let choice = restauantArr[Math.floor((Math.random() * restauantArr.length))]
     
-
+    restaurantSelection.textContent = "You should try " + choice
 }
+
+// add an event listener to the button so when it is clicked, the chooseRestaurant function is called
+restaurantBtn.addEventListener('click', chooseRestaurant)
